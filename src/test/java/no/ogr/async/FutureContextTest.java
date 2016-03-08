4d99c4bc04a0e0d8f6baad783667963f5e5e5588
Øyvind Grønnesby <ogr@ogr.no>
2016-03-09 00:28:24 +0100

    More tests

--- src/test/java/no/ogr/async/FutureContextTest.java
+++ src/test/java/no/ogr/async/FutureContextTest.java
@@ -1,6 +1,7 @@
 package no.ogr.async;
 
 import org.junit.Test;
+
 import static org.fest.assertions.Assertions.*;
 import static org.fest.assertions.Fail.*;
 
@@ -69,5 +70,56 @@ public class FutureContextTest {
                 value -> fail("Not supposed to succeed"),
                 error -> assertThat(error).isEqualTo(exception)
         );
+    }
+
+    @Test
+    public void test_future_flatmap_succeeding() {
+        final Integer integer = 42;
+        context.completed(integer).flatMap(x -> context.future(() -> x * 2)).onComplete(
+                value -> assertThat(value).isEqualTo(integer * 2),
+                error -> fail("Not supposed to get error", error)
+        );
+    }
+
+    @Test
+    public void test_future_flatmap_failing() {
+        final Integer integer = 42;
+        final RuntimeException exception = new RuntimeException("Halp");
+        context.completed(integer).flatMap(x -> context.failed(exception)).onComplete(
+                value -> fail("Not supposed to succeed"),
+                error -> assertThat(error).isEqualTo(exception)
+        );
+    }
+
+    @Test
+    public void test_future_flatmap_failing_in_mapper() {
+        final Integer integer = 42;
+        final RuntimeException exception = new RuntimeException("Halp");
+        context.completed(integer).flatMap(x -> { throw exception; }).onComplete(
+                value -> fail("Not supposed to succeed"),
+                error -> assertThat(error).isEqualTo(exception)
+        );
+    }
+
+    @Test
+    public void test_failed_future_flatmap_failing() {
+        final RuntimeException exception = new RuntimeException("Halp");
+        context.failed(exception).flatMap(x -> context.future(x::toString)).onComplete(
+                value -> fail("Not supposed to succeed"),
+                error -> assertThat(error).isEqualTo(exception)
+        );
+    }
+
+    @Test
+    public void test_promise_cant_be_fulfilled_twice() {
+        final Promise<Integer> promise = context.promise();
+        promise.fulfill(42);
+        try {
+            promise.fulfill(43);
+            fail("promise.fulfill not supposed to succeed on second execution");
+        } catch (IllegalStateException e) {
+            assertThat(e).describedAs("Promise already fulfilled");
+        }
+
     }
 }
